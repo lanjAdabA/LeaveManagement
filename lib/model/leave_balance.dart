1b3a1764b43d4ff98762e082cd8d837044e06ad1
Sarangthem Amarjit Meetei <sarangthemamarjit123@gmail.com>
2023-05-02 14:53:30 +0530

    Changes leave balance

--- lib/model/leave_balance.dart
+++ lib/model/leave_balance.dart
@@ -19,6 +19,7 @@ class LeaveBalanceModel {
   final String branch;
   final String department;
   final String designation;
+  final int leaveTypeId;
   final String leaveType;
   final String availableBalance;
 
@@ -29,6 +30,7 @@ class LeaveBalanceModel {
     required this.branch,
     required this.department,
     required this.designation,
+    required this.leaveTypeId,
     required this.leaveType,
     required this.availableBalance,
   });
@@ -38,10 +40,11 @@ class LeaveBalanceModel {
         id: json["id"],
         empCode: json["emp_code"],
         name: json["name"],
-        branch: json["branch"]!,
+        branch: json["branch"],
         department: json["department"],
-        designation: json["designation"]!,
-        leaveType: json["leave_type"]!,
+        designation: json["designation"],
+        leaveTypeId: json["leave_type_id"],
+        leaveType: json["leave_type"],
         availableBalance: json["available_balance"],
       );
 
@@ -52,6 +55,7 @@ class LeaveBalanceModel {
         "branch": branch,
         "department": department,
         "designation": designation,
+        "leave_type_id": leaveTypeId,
         "leave_type": leaveType,
         "available_balance": availableBalance,
       };
